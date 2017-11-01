7bb0cf57d109985f3cb766387b651bd13c8e1052
Daniel Pimentel <danielpimentel@Daniels-MacBook-Pro.local>
2017-10-31 22:54:03 -0500

    Polished UI, added constraints, & implemented password reset - Alpha Final

---
+++ Pods/Target Support Files/Pods-Group5Alpha/Pods-Group5Alpha-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Group5Alpha : NSObject
+@end
+@implementation PodsDummy_Pods_Group5Alpha
+@end
---
+++ Pods/Target Support Files/Pods-Group5AlphaUITests/Pods-Group5AlphaUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Group5AlphaUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Group5AlphaUITests
+@end
